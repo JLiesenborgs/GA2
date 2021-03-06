db068cce5c5a17b65741ecb357840388455f9ee9
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-06 14:52:53 +0100

    Forgot to check these in

---
+++ mpieventdistributor.cpp
@@ -0,0 +1,57 @@
+#include "mpieventdistributor.h"
+#include <string>
+
+using namespace errut;
+using namespace std;
+
+MPIEventDistributor::MPIEventDistributor(int root, MPI_Comm communicator)
+    : m_comm(communicator), m_root(root)
+{
+    m_handlers.resize(MPIEventHandler::MaxEvent);
+}
+
+MPIEventDistributor::~MPIEventDistributor()
+{
+}
+
+errut::bool_t MPIEventDistributor::setHandler(MPIEventHandler::EventType evtType,
+                                              const std::weak_ptr<MPIEventHandler> &calc)
+{
+    int evt = (int)evtType;
+    if (evt < 0 || evt >= (int)m_handlers.size())
+        return "Invalid event type " + to_string(evt);
+    m_handlers[evt] = calc;
+    return true;
+}
+
+bool_t MPIEventDistributor::signal(MPIEventHandler::EventType evtType)
+{
+    int evt = (int)evtType;
+    MPI_Bcast(&evt, 1, MPI_INT, m_root, m_comm);
+    return true;
+}
+
+bool_t MPIEventDistributor::eventLoop()
+{
+    while (true)
+    {
+        int evt = -1; // Set this to something invalid, in case we're misidentifying ourselves as root
+        MPI_Bcast(&evt, 1, MPI_INT, m_root, m_comm);
+
+        if ((MPIEventHandler::EventType)evt == MPIEventHandler::Done)
+            break;
+
+        if (evt < 0 || evt >= (int)m_handlers.size())
+            return "Received invalid event type " + to_string(evt);
+
+        if (auto handler = m_handlers[evt].lock())
+        {
+            bool_t r = handler->handleEvent((MPIEventHandler::EventType)evt);       
+            if (!r)
+                return "Error handling event " + to_string(evt) + ": " + r.getErrorString();
+        }
+        else
+            return "No handler set for event type " + to_string(evt) + "(or handler expired)";
+    }
+    return true;
+}
---
+++ mpieventdistributor.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <errut/booltype.h>
+#include <mpi.h>
+#include <memory>
+#include <vector>
+
+// To avoid circular references, no shared_ptr is used, but a weak_ptr
+
+class MPIEventHandler
+{
+public:
+    enum EventType { Done, Calculation, MaxEvent };
+
+    MPIEventHandler() { }
+    virtual ~MPIEventHandler() { }
+    virtual errut::bool_t handleEvent(EventType t) { return "Not implemented in base class"; }
+};
+
+class MPIEventDistributor
+{
+public:
+    MPIEventDistributor(int root = 0, MPI_Comm communicator = MPI_COMM_WORLD);
+    ~MPIEventDistributor();
+
+    errut::bool_t setHandler(MPIEventHandler::EventType evtType, const std::weak_ptr<MPIEventHandler> &calc);
+    errut::bool_t signal(MPIEventHandler::EventType evtType);
+    errut::bool_t eventLoop(); // Should be started on the non-root members of the communicator
+private:
+    MPI_Comm m_comm;
+    int m_root;
+    std::vector<std::weak_ptr<MPIEventHandler>> m_handlers;
+};
