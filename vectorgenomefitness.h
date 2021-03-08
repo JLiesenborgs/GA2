50599ac3ec17cc77fff6a0ac50db29722203a1bd
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-08 20:27:17 +0100

    Bugfix: was still a 'float' type which should have been template type

--- vectorgenomefitness.h
+++ vectorgenomefitness.h
@@ -5,37 +5,37 @@
 #include <cassert>
 
 template<class T>
-class VectorGenome : public ValueVector<Genome, float>
+class VectorGenome : public ValueVector<Genome, T>
 {
 public:
-	VectorGenome(size_t n = 0) : ValueVector(n) { }
-	VectorGenome(size_t n, float initValue) : ValueVector(n, initValue) { }
+	VectorGenome(size_t n = 0) : ValueVector<Genome,T>(n) { }
+	VectorGenome(size_t n, T initValue) : ValueVector<Genome,T>(n, initValue) { }
 	~VectorGenome() { }
 
 	std::shared_ptr<Genome> createCopy(bool copyContents = true) const override
 	{
-		return ValueVector::createCopy<VectorGenome<T>>(copyContents);
+		return ValueVector<Genome, T>::template createCopy<VectorGenome<T>>(copyContents);
 	}
 };
 
 template<class T>
-class VectorFitness : public ValueVector<Fitness, float>
+class VectorFitness : public ValueVector<Fitness, T>
 {
 public:
-	VectorFitness(size_t n = 0) : ValueVector(n, 0) { }
+	VectorFitness(size_t n = 0) : ValueVector<Fitness, T>(n, (T)0) { }
 	~VectorFitness() { }
 
 	std::string toString() const override
 	{
-		if (!isCalculated())
+		if (!Fitness::isCalculated())
 			return "?";
-		return ValueVector::toString();
+		return ValueVector<Fitness, T>::toString();
 	}
 
 	std::shared_ptr<Fitness> createCopy(bool copyContents = true) const override
 	{
-		auto g = ValueVector::createCopy<VectorFitness<T>>(copyContents);
-		if (copyContents && isCalculated())
+		auto g = ValueVector<Fitness, T>::template createCopy<VectorFitness<T>>(copyContents);
+		if (copyContents && Fitness::isCalculated())
 			g->setCalculated();
 		return g;
 	}
