90044f3563e58a9491d18f5ff7f1749cee26d46a
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-12 17:35:45 +0100

    Added crossover to TSP example

--- examples/travelingsalesman.cpp
+++ examples/travelingsalesman.cpp
@@ -14,36 +14,100 @@ using namespace errut;
 using namespace std;
 using namespace mogal2;
 
-class PermutationCrossover : public GenomeCrossover
+class PermutationOrderCrossover : public GenomeCrossover
 {
 public:
-    PermutationCrossover(shared_ptr<RandomNumberGenerator> rng) : m_rng(rng) { }
+    PermutationOrderCrossover(shared_ptr<RandomNumberGenerator> rng, bool twoOffspring) : m_rng(rng), m_twoOffspring(twoOffspring) { }
 
 	bool_t check(const std::vector<std::shared_ptr<Genome>> &parents) override
     {
-        // TODO
+        if (parents.size() != 2)
+            return "Need two parents";
+        for (auto g : parents)
+        {
+            if (!dynamic_cast<VectorGenome<int>*>(g.get()))
+                return "Genome is of wrong type";
+        }
         return true;
     }
 
 	bool_t generateOffspring(const std::vector<std::shared_ptr<Genome>> &parents,
-	                         std::vector<std::shared_ptr<Genome>> &generatedOffspring) override
+	                         std::vector<std::shared_ptr<Genome>> &offspring) override
     {
-        // TODO
-        return "TODO";
-    };
+        const VectorGenome<int> *pParents[2];
+        for (int i = 0 ; i < 2 ; i++)
+        {
+            pParents[i] = static_cast<const VectorGenome<int>*>(parents[i].get());
+            assert(pParents[i]);
+        }
 
+        uint32_t num = pParents[0]->getValues().size();
+        uint32_t idx1 = m_rng->getRandomUint32()%num;
+        uint32_t idx2 = m_rng->getRandomUint32()%num;
+        if (idx1 > idx2)
+            swap(idx1, idx2);
+
+        m_hasIndex.resize(num);
+        offspring.resize((m_twoOffspring)?2:1);
+        for (auto &o0 : offspring)
+        {
+            auto &v1 = pParents[0]->getValues();
+            auto &v2 = pParents[1]->getValues();
+
+            o0 = pParents[0]->createCopy(false);
+            auto o = static_cast<VectorGenome<int> *>(o0.get());
+            auto &ov = o->getValues();
+
+            for (size_t i = 0 ; i < m_hasIndex.size() ; i++)
+                m_hasIndex[i] = false;
+
+            for (uint32_t i = idx1 ; i <= idx2 ; i++)
+            {
+                ov[i] = v1[i];
+                m_hasIndex[ov[i]] = true;
+            }
+
+            uint32_t next = idx2+1;
+            for (uint32_t j = 0 ; j < v1.size() ; j++)
+            {
+                uint32_t i = (j+idx2+1)%v1.size(); // start from next index
+                int val = v2[i];
+                if (!m_hasIndex[val])
+                {
+                    ov[next%v1.size()] = val;
+                    next++;
+                }
+            }
+
+            // swap the role of the parents for the second child
+            swap(pParents[0], pParents[1]);
+        }
+
+        // cout << "parent 1 " << pParents[0]->toString() << endl;
+        // cout << "parent 2 " << pParents[1]->toString() << endl;
+        // cout << "idx = " << idx1 << " " << idx2 << endl;
+        // cout << "offspring 1" << offspring[0]->toString() << endl;
+        // if (m_twoOffspring)
+        //     cout << "offspring 2" << offspring[1]->toString() << endl;
+        // cout << endl;
+        // return "TODO";
+
+        return true;
+    };
 private:
     shared_ptr<RandomNumberGenerator> m_rng;
+    vector<bool> m_hasIndex;
+    bool m_twoOffspring;
 };
 
-class PermutationMutation : public GenomeMutation
+class PermutationSwapMutation : public GenomeMutation
 {
 public:
-    PermutationMutation(const shared_ptr<RandomNumberGenerator> &rng, double prob) : m_rng(rng), m_prob(prob) { }
+    PermutationSwapMutation(const shared_ptr<RandomNumberGenerator> &rng, double prob) : m_rng(rng), m_prob(prob) { }
 
 	bool_t check(const Genome &genome)
     {
-        VectorGenome<int> *vg = dynamic_cast<VectorGenome<int>*>(genome);
+        auto vg = dynamic_cast<const VectorGenome<int>*>(&genome);
         if (!vg)
             return "Wrong genome type";
         return true;
@@ -186,11 +250,11 @@ int main(int argc, char const *argv[])
     auto creation = make_shared<Creation>(rng, cities.size());
 
     auto calcSingle = make_shared<SingleThreadedPopulationFitnessCalculation>(make_shared<TSPFitnessCalculation>(cities));
-    auto mutation = make_shared<PermutationMutation>(rng, 0.5/cities.size());
-    auto cross = make_shared<SingleThreadedPopulationCrossover>(1.0, false,
+    auto mutation = make_shared<PermutationSwapMutation>(rng, 0.5/cities.size());
+    auto cross = make_shared<SingleThreadedPopulationCrossover>(0.1, false,
             make_shared<SimpleSortedPopulation>(make_shared<ValueFitnessComparison<double>>()),
             make_shared<RankParentSelection>(2.5, rng),
-            make_shared<PermutationCrossover>(rng),
+            make_shared<PermutationOrderCrossover>(rng, true),
             mutation,
             make_shared<SingleBestElitism>(true, mutation),
             make_shared<RemainingTargetPopulationSizeIteration>(),
@@ -202,7 +266,7 @@ int main(int argc, char const *argv[])
 
     auto r = ga.run(*creation,
                     *cross,
-                    *calcSingle, stop, 256); //, 0, 34);
+                    *calcSingle, stop, 128, 0, 258);
     if (!r)
     {
         cerr << "Error: " << r.getErrorString() << endl;
