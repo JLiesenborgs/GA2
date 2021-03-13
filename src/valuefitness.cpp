2bd015a6133518104d93be9891d646e12d963ea3
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-13 15:24:38 +0100

    Added some other MPI types

--- src/valuefitness.cpp
+++ src/valuefitness.cpp
@@ -6,5 +6,8 @@ namespace mogal2
 template<> MPI_Datatype ValueFitness<float>::m_mpiType = MPI_FLOAT;
 template<> MPI_Datatype ValueFitness<double>::m_mpiType = MPI_DOUBLE;
 template<> MPI_Datatype ValueFitness<int>::m_mpiType = MPI_INT;
+template<> MPI_Datatype ValueFitness<unsigned int>::m_mpiType = MPI_UNSIGNED;
+template<> MPI_Datatype ValueFitness<char>::m_mpiType = MPI_CHAR;
+template<> MPI_Datatype ValueFitness<unsigned char>::m_mpiType = MPI_UNSIGNED_CHAR;
 
 }
--- src/vectorgenomefitness.cpp
+++ src/vectorgenomefitness.cpp
@@ -14,4 +14,13 @@ template<> MPI_Datatype ValueVector<Fitness, double>::m_mpiType = MPI_DOUBLE;
 template<> MPI_Datatype ValueVector<Genome, int>::m_mpiType = MPI_INT;
 template<> MPI_Datatype ValueVector<Fitness, int>::m_mpiType = MPI_INT;
 
+template<> MPI_Datatype ValueVector<Genome, unsigned int>::m_mpiType = MPI_UNSIGNED;
+template<> MPI_Datatype ValueVector<Fitness, unsigned int>::m_mpiType = MPI_UNSIGNED;
+
+template<> MPI_Datatype ValueVector<Genome, char>::m_mpiType = MPI_CHAR;
+template<> MPI_Datatype ValueVector<Fitness, char>::m_mpiType = MPI_CHAR;
+
+template<> MPI_Datatype ValueVector<Genome, unsigned char>::m_mpiType = MPI_UNSIGNED_CHAR;
+template<> MPI_Datatype ValueVector<Fitness, unsigned char>::m_mpiType = MPI_UNSIGNED_CHAR;
+
 }
