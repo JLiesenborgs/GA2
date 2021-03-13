dd58e8229d1ce59fb8419164d3c71a3560503a98
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-03-13 15:25:18 +0100

    Added knapsack example

---
+++ examples/knapsack.cpp
@@ -0,0 +1,169 @@
+#include "geneticalgorithm.h"
+#include "mersennerandomnumbergenerator.h"
+#include "valuefitness.h"
+#include "vectorgenomefitness.h"
+#include "singlethreadedpopulationcrossover.h"
+#include "rankparentselection.h"
+#include "simplesortedpopulation.h"
+#include "singlebestelitism.h"
+#include "remainingtargetpopulationsizeiteration.h"
+#include "singlethreadedpopulationfitnesscalculation.h"
+#include "uniformvectorgenomecrossover.h"
+#include "vectorgenomeflipmutation.h"
+#include <iostream>
+#include <limits>
+
+using namespace errut;
+using namespace std;
+using namespace mogal2;
+
+class Creation : public GenomeFitnessCreation
+{
+public:
+    Creation(const shared_ptr<RandomNumberGenerator> rng, size_t numItems)
+     : m_rng(rng), m_numItems(numItems) { }
+
+    shared_ptr<Genome> createInitializedGenome() override
+    {
+        auto g = make_shared<VectorGenome<int>>(m_numItems);
+        
+        for (auto &x : g->getValues())
+            x = (m_rng->getRandomDouble() < 0.5)?0:1;
+
+        return g;
+    }
+    
+    shared_ptr<Fitness> createEmptyFitness() override
+    {
+        return make_shared<ValueFitness<double>>();
+    }
+private:
+    shared_ptr<RandomNumberGenerator> m_rng;
+    size_t m_numItems;
+};
+
+class Item
+{
+public:
+    Item(double val, double w) : value(val), weight(w) { }
+    double value, weight;
+};
+
+class MyStop : public FixedGenerationsStopCriterion
+{
+public:
+    MyStop(size_t n) : FixedGenerationsStopCriterion(n) { }
+    bool_t analyze(const vector<shared_ptr<Individual>> &currentBest, size_t generationNumber, bool &shouldStop) override
+    {
+        if (currentBest.size() > 0)
+        {
+            assert(currentBest.size() == 1);
+            cout << generationNumber << "| " << currentBest[0]->toString() << endl;
+        }
+        return FixedGenerationsStopCriterion::analyze(currentBest, generationNumber, shouldStop);
+    }
+};
+
+class MyGA : public GeneticAlgorithm
+{
+protected:
+    bool_t onAlgorithmDone(size_t generation, const std::vector<std::shared_ptr<Individual>> &bestIndividuals) override
+    {
+        cout << "Ending after " << generation << " generations, best are: " << endl;
+        for (auto &i : bestIndividuals)
+            cout << i->toString() << endl;
+        return true;
+    }
+};
+
+class KnapsackFitnessCalculation : public GenomeFitnessCalculation
+{
+public:
+    KnapsackFitnessCalculation(const vector<Item> &items, double weightLimit)
+     : m_items(items), m_weightLimit(weightLimit) { }
+    bool_t calculate(const Genome &genome, Fitness &fitness) override
+    {
+        const VectorGenome<int> &vg = static_cast<const VectorGenome<int> &>(genome);
+        const vector<int> &indices = vg.getValues();
+        ValueFitness<double> &vf = static_cast<ValueFitness<double> &>(fitness);
+
+        double totalValue = 0;
+        double totalWeight = 0;
+
+        assert(m_items.size() == vg.getValues().size());
+        for (size_t i = 0 ; i < vg.getValues().size() ; i++)
+        {
+            totalValue += vg.getValues()[i]*m_items[i].value;
+            totalWeight += vg.getValues()[i]*m_items[i].weight;
+        }
+
+        if (totalWeight > m_weightLimit)
+        {
+            // In case all genomes are bad, we do want some incentive to go below the weight
+            // limit
+            totalValue = -(totalWeight-m_weightLimit);
+        }
+        vf.setValue(totalValue);
+        return true;
+    }
+private:
+    vector<Item> m_items;
+    double m_weightLimit;
+};
+
+int main(int argc, char const *argv[])
+{
+    size_t numItems = 28;
+    vector<Item> items;
+    double targetWeight = 0; // fill in later
+
+    // Initialize the items randomly, but with fixed seeds to that different runs
+    // use the same items
+    {
+        auto rng0 = make_shared<MersenneRandomNumberGenerator>(1337);
+        double allWeight = 0;
+        
+        for (size_t i = 0 ; i < numItems ; i++)
+        {
+            double value = (int)(rng0->getRandomDouble()*100.0)+1;
+            double weight = (int)(rng0->getRandomDouble()*10.0)+1;
+
+            items.push_back({value, weight});
+            allWeight += weight;
+        }
+
+        targetWeight = allWeight/4.0;
+    }
+
+    random_device rd;
+    unsigned int seed = rd();
+    auto rng = make_shared<MersenneRandomNumberGenerator>(seed);
+
+    auto creation = make_shared<Creation>(rng, items.size());
+
+    auto calcSingle = make_shared<SingleThreadedPopulationFitnessCalculation>(make_shared<KnapsackFitnessCalculation>(items, targetWeight));
+    auto mutation = make_shared<VectorGenomeFlipMutation<int>>(rng, 1.0/items.size());
+    auto cross = make_shared<SingleThreadedPopulationCrossover>(0.1, false,
+            make_shared<SimpleSortedPopulation>(make_shared<ValueFitnessComparison<double,false>>()),
+            make_shared<RankParentSelection>(2.5, rng),
+            make_shared<UniformVectorGenomeCrossover<int>>(rng, false),
+            mutation,
+            make_shared<SingleBestElitism>(true, mutation),
+            make_shared<RemainingTargetPopulationSizeIteration>(),
+            rng
+        );
+    
+    MyStop stop(1000);
+    MyGA ga;
+
+    auto r = ga.run(*creation,
+                    *cross,
+                    *calcSingle, stop, 128);
+    if (!r)
+    {
+        cerr << "Error: " << r.getErrorString() << endl;
+        return -1;
+    }
+    cout << "Done" << endl;
+    return 0;
+}
---
+++ src/vectorgenomeflipmutation.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "mogal2config.h"
+#include "crossovermutation.h"
+#include "vectorgenomefitness.h"
+
+namespace mogal2
+{
+
+template<class T>
+class VectorGenomeFlipMutation : public GenomeMutation
+{
+public:
+    VectorGenomeFlipMutation(const std::shared_ptr<RandomNumberGenerator> &rng, double prob)
+     : m_rng(rng), m_prob(prob)
+    {
+    }
+
+    errut::bool_t check(const Genome &genome) override
+    {
+        auto g = dynamic_cast<const VectorGenome<T> *>(&genome);
+        if (!g)
+            return "Genome type is incompatible";
+        return true;
+    }
+
+	errut::bool_t mutate(Genome &genome, bool &isChanged) override
+    {
+        VectorGenome<T> &g = static_cast<VectorGenome<T> &>(genome);
+        auto &v = g.getValues();
+
+        for (auto &x : v)
+        {
+            if (m_rng->getRandomDouble() < m_prob)
+                x = !x;
+        }
+        return true;
+    }
+private:
+    std::shared_ptr<RandomNumberGenerator> m_rng;
+    double m_prob;
+};
+
+}
