34a86ed6e79378d9a3482a6a808d8cf2b58d657c
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2021-11-16 14:33:13 +0100

    Added various test functions to the basic jade code

--- tests/jadebasictest.cpp
+++ tests/jadebasictest.cpp
@@ -27,6 +27,361 @@ struct f1_Sphere : public Problem
 	}
 };
 
+template<size_t D>
+struct f2 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0, prod = 1;
+		for (auto v : x)
+		{
+			sum += std::abs(v);
+			prod *= std::abs(v);
+		}
+		return sum + prod;
+	}
+};
+
+template<size_t D>
+struct f3 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0;
+		
+		for (size_t i = 0 ; i < D ; i++)
+		{
+			double sub = 0;
+			for (size_t j = 0 ; j <= i ; j++)
+				sub += x[j];
+			sum += sub*sub;
+		}
+		return sum;
+	}
+};
+
+template<size_t D>
+struct f4 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double max = -numeric_limits<double>::max();
+		for (auto v : x)
+			if (std::abs(v) > max)
+				max = std::abs(v);
+		return max;
+	}
+};
+
+template<size_t D>
+struct f5 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0;
+		for (size_t i = 0 ; i < D-1 ; i++)
+			sum += 100.0*(x[i+1]-x[i]*x[i])*(x[i+1]-x[i]*x[i]) + (x[i] - 1.0)*(x[i] - 1.0);
+		return sum;
+	}
+};
+
+template<size_t D>
+struct f6 : public Problem
+{
+public:
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0;
+		for (auto v : x)
+			sum += std::floor(v+0.5)*std::floor(v+0.5);
+		return sum;
+	}
+};
+
+template<size_t D>
+struct f7 : public Problem // Doesn't seem to work well
+{
+	f7(mt19937 &rng) : m_rng(rng) { }
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0;
+		for (size_t i = 0 ; i < D ; i++)
+			sum += (i+1.0)*x[i]*x[i]*x[i]*x[i];
+		return sum + (uniform_real_distribution<>(0,1))(m_rng);
+	}
+private:
+	mt19937 &m_rng;
+};
+
+template<size_t D>
+struct f8 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+
+		double sum = 0;
+		for (auto v : x)
+			sum += -v*std::sin(std::sqrt(std::abs(v)));
+		return sum + 418.98288727243369*D;
+	}
+};
+
+template<size_t D>
+struct f9 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0;
+		for (auto v : x)
+			sum += v*v -10.0*std::cos(2.0*M_PI*v) + 10.0;
+		return sum;
+	}
+};
+
+template<size_t D>
+struct f10 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum1 = 0, sum2 = 0;
+		for (auto v : x)
+		{
+			sum1 += v*v;
+			sum2 += std::cos(2.0*M_PI*v);
+		}
+
+		return -20.0*std::exp(-0.2*std::sqrt(sum1/D)) - std::exp(sum2/D) + 20 + std::exp(1);
+	}
+};
+
+template<size_t D>
+struct f11 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		double sum = 0, prod = 1;
+		for (size_t i = 0 ; i < D ; i++)
+		{
+			sum += x[i]*x[i];
+			prod *= std::cos(x[i]/std::sqrt(i+1.0));
+		}
+		return sum/4000.0 - prod + 1.0;
+	}
+};
+
+inline double u(double z, double a, double k, double m)
+{
+	if (z > a)
+		return k*std::pow(z-a, m);
+	if (z < -a)
+		return k*std::pow(-z-a, m);
+	return 0;
+};
+
+template<size_t D>
+struct f12 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+		auto y = [&x](size_t i) { return 1.0+0.25*(x[i] + 1.0); };
+
+		double sum = 10.0*std::sin(M_PI*y(0))*std::sin(M_PI*y(0));
+
+		for (size_t i = 0 ; i < D-1 ; i++)
+			sum += (y(i)-1.0)*(y(i)-1.0)*(1.0+10.0*std::sin(M_PI*y(i+1))*std::sin(M_PI*y(i+1)));
+
+		sum += (y(D-1)-1.0)*(y(D-1)-1.0);
+		sum *= M_PI/D;
+
+		for (size_t i = 0 ; i < D ; i++)
+			sum += u(x[i], 10, 100, 4);
+		return sum;
+	}
+};
+
+template<size_t D>
+struct f13 : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == D);
+
+		double sum = std::sin(3*M_PI*x[0])*std::sin(3*M_PI*x[0]);
+
+		for (size_t i = 0 ; i < D-1 ; i++)
+			sum += (x[i] - 1.0)*(x[i] - 1.0)*(1.0+std::sin(3*M_PI*x[i+1])*std::sin(3*M_PI*x[i+1]));
+
+		sum += (x[D-1]-1.0)*(x[D-1]-1.0)*(1.0*std::sin(2*M_PI*x[D-1])*std::sin(2*M_PI*x[D-1]));
+		sum *= 0.1;
+
+		for (size_t i = 0 ; i < D ; i++)
+			sum += u(x[i], 5, 100, 4);
+
+		return sum;
+	}
+};
+
+struct f14_Branin : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == 2);
+		const double a = 1.0;
+		const double b = 5.1/(4.0*M_PI*M_PI);
+		const double c = 5.0/M_PI;
+		const double r = 6.0;
+		const double s = 10.0;
+		const double t = 1.0/(8.0*M_PI);
+
+		return a*std::pow(x[1] - b*x[0]*x[0] + c*x[0] - r,2) + s*(1.0-t)*std::cos(x[0]) + s;
+	}
+};
+
+struct f15_GoldsteinPrice : public Problem
+{
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == 2);
+
+		return (1.0+std::pow(x[0]+x[1]+1.0,2)*(19.0-14.0*x[0]+3.0*x[0]*x[0]-14.0*x[1]+6.0*x[0]*x[1]+3.0*x[1]*x[1]))*(
+			30.0+std::pow(2.0*x[0]-3.0*x[1],2)*(18.0-32.0*x[0]+12.0*x[0]*x[0]+48.0*x[1]-36.0*x[0]*x[1]+27.0*x[1]*x[1])
+		);
+	}
+};
+
+struct f16_Hartman3D : public Problem
+{
+	f16_Hartman3D()
+	{
+		alpha = { 1.0, 1.2, 3.0, 3.2 };
+		A = {
+			{ 3.0, 10.0, 30.0},
+			{ 0.1, 10.0, 35.0},
+			{ 3.0, 10.0, 30.0},
+			{ 0.1, 10.0, 35.0}
+		};
+
+		P = {
+			{ 3689, 1170, 2673 },
+			{ 4699, 4387, 7470 },
+			{ 1091, 8732, 5547 },
+			{  381, 5743, 8828 }
+		};
+		for (auto &v : P)
+			for (auto &x : v)
+				x *= 1e-4;
+	}
+
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == 3);
+		double sum = 0;
+		for (size_t i = 0 ; i < 4 ; i++)
+		{
+			double s = 0;
+			for (size_t j = 0 ; j < 3 ; j++)
+				s += A[i][j]*std::pow(x[j]-P[i][j],2);
+
+			sum += alpha[i]*std::exp(-s);
+		}
+		return -sum;
+	}
+private:
+	vector<double> alpha;
+	vector<vector<double>> A, P;
+};
+
+struct f17_Hartman6D : public Problem
+{
+	f17_Hartman6D()
+	{
+		alpha = { 1.0, 1.2, 3.0, 3.2 };
+		A = {
+			{ 10, 3, 17, 3.5, 1.7, 8 },
+			{ 0.05, 10, 17, 0.1, 8, 14 },
+			{ 3, 3.5, 1.7, 10, 17, 8 },
+			{ 17, 8, 0.05, 10, 0.1, 14 }
+		};
+
+		P = {
+			{ 1312, 1696, 5569, 124, 8283, 5886 },
+			{ 2329, 4135, 8307, 3736, 1004, 9991 },
+			{ 2348, 1451, 3522, 2883, 3047, 6650 },
+			{ 4047, 8828, 8732, 5743, 1091, 381 }
+		};
+		for (auto &v : P)
+			for (auto &x : v)
+				x *= 1e-4;
+	}
+
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == 6);
+		double sum = 0;
+		for (size_t i = 0 ; i < 4 ; i++)
+		{
+			double s = 0;
+			for (size_t j = 0 ; j < 6 ; j++)
+				s += A[i][j]*std::pow(x[j]-P[i][j],2);
+
+			sum += alpha[i]*std::exp(-s);
+		}
+		return -sum;
+	}
+private:
+	vector<double> alpha;
+	vector<vector<double>> A, P;
+};
+
+struct f_Shekel : public Problem
+{
+public:
+	f_Shekel(size_t m) : m_m(m)
+	{
+		beta = { 1, 2, 2, 4, 4, 6, 3, 7, 5, 5 };
+		for (auto &b : beta)
+			b *= 0.1;
+		
+		C = {
+			{ 4, 1, 8, 6, 3, 2, 5, 8, 6, 7 },
+			{ 4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6 },
+			{ 4, 1, 8, 6, 3, 2, 5, 8, 6, 7 },
+			{ 4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6 }
+		};
+	}
+
+	double evaluate(const vector<double> &x) override
+	{
+		assert(x.size() == 4);
+		assert(m_m <= 10);
+		double sum = 0;
+		for (size_t i = 0 ; i < m_m ; i++)
+		{
+			double s = beta[i];
+			for (size_t j = 0 ; j < 4 ; j++)
+				s += std::pow(x[j] - C[j][i],2);
+
+			sum += 1.0/s;
+		}
+		return -sum;
+	}
+private:
+	const size_t m_m;
+	vector<double> beta;
+	vector<vector<double>> C;
+};
+
 inline vector<double> generateIndividual(mt19937 &rng, const vector<pair<double,double>> &IPR)
 {
 	vector<double> x(IPR.size());
@@ -50,6 +405,9 @@ inline double clip(double x, double x0, double x1)
 
 inline double meanA(const vector<double> &P)
 {
+	if (P.size() == 0)
+		return 0;
+
 	double sum = 0;
 	for (auto x : P)
 		sum += x;
@@ -59,6 +417,9 @@ inline double meanA(const vector<double> &P)
 
 inline double meanL(const vector<double> &P)
 {
+	if (P.size() == 0)
+		return 0;
+
 	double sum = 0, sum2 = 0;
 	for (auto x : P)
 	{
@@ -77,6 +438,10 @@ jade(mt19937 &rng, Problem &problem, size_t NP, const vector<pair<double,double>
 	assert(NP > 4);
 	assert(p >= 0 && p <= 1);
 	
+	bool dumpPop = false;
+	if (getenv("DUMPPOP"))
+		dumpPop = true;
+
 	size_t evaluations = 0;
 	double muF = 0.5, muCR = 0.5;
 	vector<pair<vector<double>,double>> P(NP);
@@ -89,6 +454,23 @@ jade(mt19937 &rng, Problem &problem, size_t NP, const vector<pair<double,double>
 		evaluations++;
 	}
 	sort(P.begin(), P.end(), [](auto &i, auto &j) { return i.second < j.second; });
+
+	auto logPop = [&P, dumpPop](size_t gen)
+	{
+		if (!dumpPop)
+			return;
+
+		cerr << "Generation " << gen << endl;
+		for (size_t i = 0 ; i < P.size() ; i++)
+		{
+			cerr << "(" << i << ") [";
+			for (auto x : P[i].first)
+				cerr << " " << x;
+			cerr << " ] => " << P[i].second << endl;
+		}
+	};
+
+	logPop(0);
 
 	vector<vector<double>> A;
 	vector<double> SF, SCR;
@@ -116,13 +498,19 @@ jade(mt19937 &rng, Problem &problem, size_t NP, const vector<pair<double,double>
 			if (xR2 < NP) // choose r2 from main population
 			{
 				for(size_t j = 0 ; j < v.size() ; j++)
+				{
 					v[j] = xi[j] + Fi*(P[xBest].first[j] - xi[j]) + Fi*(P[xR1].first[j] - P[xR2].first[j]);
+					assert(!isnan(v[j]));
+				}
 			}
 			else // choose r2 from archive
 			{
 				assert(xR2 - NP < A.size());
 				for(size_t j = 0 ; j < v.size() ; j++)
+				{
 					v[j] = xi[j] + Fi*(P[xBest].first[j] - xi[j]) + Fi*(P[xR1].first[j] - A[xR2-NP][j]);
+					assert(!isnan(v[j]));
+				}
 			}
 
 			vector<double> u(v.size());
@@ -180,10 +568,12 @@ jade(mt19937 &rng, Problem &problem, size_t NP, const vector<pair<double,double>
 			A.resize(A.size()-1);
 		}
 
+		// What to do if these sets are empty? Use 0 as mean? Don't update?
 		muCR = (1.0-c)*muCR + c*meanA(SCR);
-		muF = (1.0-c)*muF + c* meanL(SF);
+		muF = (1.0-c)*muF + c*meanL(SF);
 
 		sort(P.begin(), P.end(), [](auto &i, auto &j) { return i.second < j.second; });
+		logPop(g+1);
 	}
 
 	return { false, P[0].first, P[0].second, maxGen, evaluations, muLog };
@@ -203,7 +593,15 @@ struct Test
 int main(int argc, char const *argv[])
 {
 	random_device rd;
-	mt19937 rng(rd());
+	unsigned int seed = rd();
+	if (getenv("SEED"))
+		seed = (unsigned int)stoul(getenv("SEED"));
+
+	mt19937 rng(seed);
+
+	string matchName;
+	if (argc > 1)
+		matchName = argv[1];
 
 	bool archive = true;
 	if (getenv("NOARCH"))
@@ -212,6 +610,37 @@ int main(int argc, char const *argv[])
 	vector<Test> tests {
 		{ "f1_Sphere_30", make_shared<f1_Sphere<30>>(), vector<pair<double,double>>(30,{-100,100}), 1e-8, 100, 100000, false },
 		{ "f1_Sphere_100", make_shared<f1_Sphere<100>>(), vector<pair<double,double>>(100,{-100,100}), 1e-8, 400, 100000, false },
+		{ "f2_30", make_shared<f2<30>>(), vector<pair<double,double>>(30,{-10,10}), 1e-8, 100, 100000, false },
+		{ "f2_100", make_shared<f2<100>>(), vector<pair<double,double>>(100,{-10,10}), 1e-8, 400, 100000, false },
+		{ "f3_30", make_shared<f3<30>>(), vector<pair<double,double>>(30,{-100,100}), 1e-8, 100, 100000, false },
+		{ "f3_100", make_shared<f3<100>>(), vector<pair<double,double>>(100,{-100,100}), 1e-8, 400, 100000, false },
+		{ "f4_30", make_shared<f4<30>>(), vector<pair<double,double>>(30,{-100,100}), 1e-8, 100, 100000, false },
+		{ "f4_100", make_shared<f4<100>>(), vector<pair<double,double>>(100,{-100,100}), 1e-8, 400, 100000, false },
+		{ "f5_30", make_shared<f5<30>>(), vector<pair<double,double>>(30,{-100,100}), 1e-8, 100, 100000, false },
+		{ "f5_100", make_shared<f5<100>>(), vector<pair<double,double>>(100,{-100,100}), 1e-8, 400, 100000, false },
+		{ "f6_30", make_shared<f6<30>>(), vector<pair<double,double>>(30,{-100,100}), 1e-8, 100, 100000, false },
+		{ "f6_100", make_shared<f6<100>>(), vector<pair<double,double>>(100,{-100,100}), 1e-8, 400, 100000, false },
+		// { "f7_30", make_shared<f7<30>>(rng), vector<pair<double,double>>(30,{-1.28,1.28}), 1e-2, 100, 100000, false },
+		// { "f7_100", make_shared<f7<100>>(rng), vector<pair<double,double>>(100,{-1.28,1.28}), 1e-2, 400, 100000, false },
+		{ "f8_30", make_shared<f8<30>>(), vector<pair<double,double>>(30,{-500,500}), 1e-8, 100, 100000, true },
+		{ "f8_100", make_shared<f8<100>>(), vector<pair<double,double>>(100,{-500,500}), 1e-8, 400, 100000, true },
+		{ "f9_30", make_shared<f9<30>>(), vector<pair<double,double>>(30,{-5.12,5.12}), 1e-8, 100, 100000, false },
+		{ "f9_100", make_shared<f9<100>>(), vector<pair<double,double>>(100,{-5.12,5.12}), 1e-8, 400, 100000, false },
+		{ "f10_30", make_shared<f10<30>>(), vector<pair<double,double>>(30,{-32,32}), 1e-8, 100, 100000, false },
+		{ "f10_100", make_shared<f10<100>>(), vector<pair<double,double>>(100,{-32,32}), 1e-8, 400, 100000, false },
+		{ "f11_30", make_shared<f11<30>>(), vector<pair<double,double>>(30,{-600,600}), 1e-8, 100, 100000, false },
+		{ "f11_100", make_shared<f11<100>>(), vector<pair<double,double>>(100,{-600,600}), 1e-8, 400, 100000, false },
+		{ "f12_30", make_shared<f12<30>>(), vector<pair<double,double>>(30,{-50,50}), 1e-8, 100, 100000, false },
+		{ "f12_100", make_shared<f12<100>>(), vector<pair<double,double>>(100,{-50,50}), 1e-8, 400, 100000, false },
+		{ "f13_30", make_shared<f13<30>>(), vector<pair<double,double>>(30,{-50,50}), 1e-8, 100, 100000, false },
+		{ "f13_100", make_shared<f13<100>>(), vector<pair<double,double>>(100,{-50,50}), 1e-8, 400, 100000, false },
+		{ "f14_Branin", make_shared<f14_Branin>(), { {-5, 10}, {0, 15} }, 0.3978873577 + 1e-8, 30, 100000, false },
+		{ "f15_GoldsteinPrice", make_shared<f15_GoldsteinPrice>(), { { -2, 2 }, { -2, 2 } }, 3.0 + 1e-8, 30, 100000, false },
+		{ "f16_Hartman3D", make_shared<f16_Hartman3D>(), {{0,1},{0,1},{0,1}}, -3.862779787 + 1e-8, 30, 100000, false },
+		{ "f17_Hartman6D", make_shared<f17_Hartman6D>(), {{0,1},{0,1},{0,1},{0,1},{0,1},{0,1}}, -3.322368011 + 1e-8, 30, 100000, false },
+		{ "f18_Shekel5", make_shared<f_Shekel>(5), {{0,10},{0,10},{0,10},{0,10}}, -10.153199679 + 1e-8, 30, 100000, false },
+		{ "f19_Shekel7", make_shared<f_Shekel>(7), {{0,10},{0,10},{0,10},{0,10}}, -10.402915336 + 1e-8, 30, 100000, false },
+		{ "f20_Shekel10", make_shared<f_Shekel>(10), {{0,10},{0,10},{0,10},{0,10}}, -10.536443153 + 1e-8, 30, 100000, false },
 	};
 	
 	size_t numRuns = 20;
@@ -222,8 +651,14 @@ int main(int argc, char const *argv[])
 	if (getenv("DUMPFINAL"))
 		printBest = true;
 
+	size_t testCount = 0;
 	for (auto &test : tests)
 	{
+		if (matchName.length() > 0 && matchName != test.name)
+			continue;
+
+		testCount++;
+
 		size_t successCount = 0;
 		size_t totalEvals = 0;
 
@@ -262,6 +697,13 @@ int main(int argc, char const *argv[])
 			cout << " no archive";
 
 		cout << endl;
+	}
+
+	if (testCount == 0)
+	{
+		cerr << "No tests run, known are:" << endl;
+		for (const auto &test : tests)
+			cerr << "  " << test.name << endl;
 	}
 	return 0;
 }
