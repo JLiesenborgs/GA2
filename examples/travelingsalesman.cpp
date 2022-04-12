f100d9fde08de7c32742bc4156914f36ee3518e5
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2022-04-12 16:14:47 +0200

    Added random cities to example

--- examples/travelingsalesman.cpp
+++ examples/travelingsalesman.cpp
@@ -76,14 +76,21 @@ public:
 
 class MyGA : public EvolutionaryAlgorithm
 {
+public:
+	double getBestFitness() const { return static_cast<ValueFitness<double>&>(m_best->fitnessRef()).getValue(); }
+	const VectorGenome<int> &getBestGenome() const { return static_cast<const VectorGenome<int>&>(m_best->genomeRef()); }
 protected:
 	bool_t onAlgorithmDone(size_t generation, const std::vector<std::shared_ptr<Individual>> &bestIndividuals) override
 	{
 		cout << "Ending after " << generation << " generations, best are: " << endl;
 		for (auto &i : bestIndividuals)
 			cout << i->toString() << endl;
+
+		m_best = bestIndividuals[0]->createCopy();
 		return true;
 	}
+
+	shared_ptr<Individual> m_best;
 };
 
 class TSPFitnessCalculation : public GenomeFitnessCalculation
@@ -114,14 +121,58 @@ private:
 	vector<City> m_cities;
 };
 
-int main(int argc, char const *argv[])
+void usage()
 {
-	size_t numCities = 28;
+	cout << R"XYZ(
+travelingsalesman circle numcities
+
+or
+
+travelingsalesman random numcities seed
+)XYZ";
+	exit(-1);
+}
+
+vector<City> getRandomCities(unsigned int seed, size_t numCities)
+{
 	vector<City> cities;
+	MersenneRandomNumberGenerator rng(seed);
+
 	for (size_t i = 0 ; i < numCities ; i++)
 	{
-		double theta = (double)i/(double)numCities * 2.0*M_PI;
-		cities.push_back(City { cos(theta), sin(theta) } );
+		double x = rng.getRandomDouble();
+		double y = rng.getRandomDouble();
+		cities.push_back({x, y});
+	}
+	return cities;
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc < 3)
+		usage();
+
+	string type(argv[1]);
+	size_t numCities = stoul(argv[2]);
+	vector<City> cities;
+
+	if (type == "circle")
+	{
+		if (argc != 3)
+			usage();
+
+		for (size_t i = 0 ; i < numCities ; i++)
+		{
+			double theta = (double)i/(double)numCities * 2.0*M_PI;
+			cities.push_back(City { cos(theta), sin(theta) } );
+		}
+	}
+	else if (type == "random")
+	{
+		if (argc != 4)
+			usage();
+
+		cities = getRandomCities((unsigned int)stoul(argv[3]), numCities);
 	}
 
 	random_device rd;
@@ -162,5 +213,13 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 	cout << "Done" << endl;
+
+	const VectorGenome<int> &cityOrder = ga.getBestGenome();
+	for (int idx : cityOrder.getValues())
+		cerr << cities[idx].x << " " << cities[idx].y << endl;
+
+	int idx = cityOrder.getValues()[0];
+	cerr << cities[idx].x << " " << cities[idx].y << endl;
+
 	return 0;
 }
