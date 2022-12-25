89a74c0802c7a296849152086eedb67b32fe0328
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2022-12-25 17:34:38 +0100

    Functions needed to be virtual - renaming class
    Idea is to use the singlepopulationcrossover class for only one
    population, and create a wrapper class for multiple populations.
    That will allow to use entirely different algorithms for the individual
    populations, as long as their individuals can be swapped.

--- src/migrationstrategy.h
+++ src/migrationstrategy.h
@@ -11,13 +11,13 @@ public:
 	MigrationStrategy() { }
 	virtual ~MigrationStrategy() { }
 
-	errut::bool_t check(const std::vector<std::shared_ptr<Population>> &populations) { return "Not implemented in base class"; } 
+	virtual errut::bool_t check(const std::vector<std::shared_ptr<Population>> &populations) { return "Not implemented in base class"; } 
 	// Note that this is run after the generation has been updated (so generation will
 	// be at least 1), and when
 	// all fitness values have been calculated, in case the migration strategy
 	// wants to take this into account. The individuals are not sorted in any
 	// way at this point
-	errut::bool_t migrate(size_t generation, std::vector<std::shared_ptr<Population>> &populations) { return "Not implemented in base class"; }
+	virtual errut::bool_t migrate(size_t generation, std::vector<std::shared_ptr<Population>> &populations) { return "Not implemented in base class"; }
 };
 
 }
