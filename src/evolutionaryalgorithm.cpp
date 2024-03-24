f94fd5f7a20ca7fe7589b45be5b1fcc308f6d9b2
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-03-24 22:17:38 +0100

    Allowing access to number of generations executed and population

--- src/evolutionaryalgorithm.cpp
+++ src/evolutionaryalgorithm.cpp
@@ -7,6 +7,7 @@ namespace eatk
 {
 
 EvolutionaryAlgorithm::EvolutionaryAlgorithm()
+	: m_generation(0)
 {
 }
 
@@ -42,7 +43,9 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 							 size_t maxPopulationSize)
 {
 	bool_t r;
-	auto population = make_shared<Population>();
+	m_population = make_shared<Population>();
+	m_populations = { m_population };
+
 	auto newPopulation = make_shared<Population>();
 	auto refFitness = gfc.createEmptyFitness();
 	auto refIndividual = gfc.createReferenceIndividual();
@@ -59,30 +62,30 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 	if (maxPopulationSize == 0)
 		maxPopulationSize = popSize;
 
-	size_t generation = 0;
+	m_generation = 0;
 
-	if (!(r = initializePopulation(popSize, population, gfc, refIndividual, refFitness, generation)))
+	if (!(r = initializePopulation(popSize, m_population, gfc, refIndividual, refFitness, m_generation)))
 		return r;
 
-	auto beforeFitnessCalculatedCallback = [&generation, &population, this]() -> bool_t
+	auto beforeFitnessCalculatedCallback = [this]() -> bool_t
 	{
 		bool_t r;
-		if (!(r = onBeforeFitnessCalculation(generation, population)))
-			return "Error inspecting population before fitness calculation in generation " + to_string(generation) + ": " + r.getErrorString();
+		if (!(r = onBeforeFitnessCalculation(m_generation, m_population)))
+			return "Error inspecting population before fitness calculation in generation " + to_string(m_generation) + ": " + r.getErrorString();
 		return true;
 	};
 
 	if (!(r = beforeFitnessCalculatedCallback()))
 		return r;
 
-	if (!(r = fitnessCalc.calculatePopulationFitness({population})))
+	if (!(r = fitnessCalc.calculatePopulationFitness({m_population})))
 		return "Error calculating fitness: " + r.getErrorString();
 
-	auto fitnessCalculatedCallback = [&generation, &population, this]() -> bool_t
+	auto fitnessCalculatedCallback = [this]() -> bool_t
 	{
 		bool_t r;
-		if (!(r = onFitnessCalculated(generation, population)))
-			return "Error inspecting population after fitness calculation in generation " + to_string(generation) + ": " + r.getErrorString();
+		if (!(r = onFitnessCalculated(m_generation, m_population)))
+			return "Error inspecting population after fitness calculation in generation " + to_string(m_generation) + ": " + r.getErrorString();
 		return true;
 	};
 
@@ -91,26 +94,26 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 
 	while (true)
 	{		
-		if (generation == 0)
+		if (m_generation == 0)
 		{
-			if (!(r = evolver.check(population)))
+			if (!(r = evolver.check(m_population)))
 				return "Error in population evolver check: " + r.getErrorString();
 		}
-		if (!(r = evolver.createNewPopulation(generation, population, popSize)))
+		if (!(r = evolver.createNewPopulation(m_generation, m_population, popSize)))
 			return "Error creating new population: " + r.getErrorString();
 
-		const size_t curPopSize = population->size();
+		const size_t curPopSize = m_population->size();
 		if (curPopSize > maxPopulationSize)
 			return "Population size (" + to_string(curPopSize) + ") exceeds maximum (" + to_string(maxPopulationSize) + ")";
 		if (curPopSize < minPopulationSize)
 			return "Population size (" + to_string(curPopSize) + ") is less than minimum (" + to_string(minPopulationSize) + ")";
 
-		generation++; // At this point we can call it the new generation
+		m_generation++; // At this point we can call it the new generation
 
 		if (!(r = beforeFitnessCalculatedCallback()))
 			return r;
 
-		if (!(r = fitnessCalc.calculatePopulationFitness({population})))
+		if (!(r = fitnessCalc.calculatePopulationFitness({m_population})))
 			return "Error calculating fitness: " + r.getErrorString();
 
 		if (!(r = fitnessCalculatedCallback()))
@@ -118,13 +121,13 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 
 		bool shouldStop = false;
 
-		if (!(r = stopCriterion.analyze(evolver, generation, shouldStop)))
+		if (!(r = stopCriterion.analyze(evolver, m_generation, shouldStop)))
 			return "Error in termination check: " + r.getErrorString();
 		if (shouldStop)
 			break;
 	}
 
-	if (!(r = onAlgorithmDone(generation, evolver.getBestIndividuals())))
+	if (!(r = onAlgorithmDone(m_generation, evolver.getBestIndividuals())))
 		return "Error inspecting best individuals upon algorithm end: " + r.getErrorString();
 
 	return true;
@@ -170,56 +173,57 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 	if (maxPopulationSizes.size() != popSizes.size())
 		return "The number of maximum population sizes does not match the number of populations";
 
-	vector<shared_ptr<Population>> populations(popSizes.size());
+	m_population = nullptr; // Is there something sensible to do? 
+	m_populations.resize(popSizes.size());
 	vector<shared_ptr<Population>> newPopulations(popSizes.size());
 
-	size_t generation = 0;
+	m_generation = 0;
 
 	for (size_t p = 0 ; p < popSizes.size() ; p++)
 	{
-		populations[p] = make_shared<Population>();
+		m_populations[p] = make_shared<Population>();
 		newPopulations[p] = make_shared<Population>();
-		if (!(r = initializePopulation(popSizes[p], populations[p], gfc, refIndividual, refFitness, generation)))
+		if (!(r = initializePopulation(popSizes[p], m_populations[p], gfc, refIndividual, refFitness, m_generation)))
 			return r;
 	}
 
-	auto beforeFitnessCalculatedCallback = [&generation, &populations, this]() -> bool_t
+	auto beforeFitnessCalculatedCallback = [this]() -> bool_t
 	{
 		bool_t r;
-		if (!(r = onBeforeFitnessCalculation(generation, populations)))
-			return "Error inspecting population before fitness calculation in generation " + to_string(generation) + ": " + r.getErrorString();
+		if (!(r = onBeforeFitnessCalculation(m_generation, m_populations)))
+			return "Error inspecting population before fitness calculation in generation " + to_string(m_generation) + ": " + r.getErrorString();
 		return true;
 	};
 
 	if (!(r = beforeFitnessCalculatedCallback()))
 		return r;
 
-	if (!(r = fitnessCalc.calculatePopulationFitness(populations)))
+	if (!(r = fitnessCalc.calculatePopulationFitness(m_populations)))
 		return "Error calculating fitness: " + r.getErrorString();
 
-	auto fitnessCalculatedCallback = [&generation, &populations, this]() -> bool_t
+	auto fitnessCalculatedCallback = [this]() -> bool_t
 	{
 		bool_t r;
-		if (!(r = onFitnessCalculated(generation, populations)))
-			return "Error inspecting population after fitness calculation in generation " + to_string(generation) + ": " + r.getErrorString();
+		if (!(r = onFitnessCalculated(m_generation, m_populations)))
+			return "Error inspecting population after fitness calculation in generation " + to_string(m_generation) + ": " + r.getErrorString();
 		return true;
 	};
 
 	if (!(r = fitnessCalculatedCallback()))
 		return r;
 	
-	auto popSizeCheck = [&populations, &popSizes, &maxPopulationSizes, &minPopulationSizes]() -> bool_t
+	auto popSizeCheck = [this, &popSizes, &maxPopulationSizes, &minPopulationSizes]() -> bool_t
 	{
-		if (populations.size() != maxPopulationSizes.size())
-			return "Number of populations changed! Was " + to_string(maxPopulationSizes.size()) + " and is now " + to_string(populations.size());
+		if (m_populations.size() != maxPopulationSizes.size())
+			return "Number of populations changed! Was " + to_string(maxPopulationSizes.size()) + " and is now " + to_string(m_populations.size());
 
-		for (size_t p = 0 ; p < populations.size() ; p++)
+		for (size_t p = 0 ; p < m_populations.size() ; p++)
 		{
-			auto &population = populations[p];
+			auto &curPop = m_populations[p];
 			size_t maxPopulationSize = maxPopulationSizes[p];
 			size_t minPopulationSize = minPopulationSizes[p];
 
-			const size_t curPopSize = population->size();
+			const size_t curPopSize = curPop->size();
 			if (curPopSize > maxPopulationSize)
 				return "Population size (" + to_string(curPopSize) + ") for population (" + to_string(p) + ") exceeds maximum (" + to_string(maxPopulationSize) + ")";
 			if (curPopSize < minPopulationSize)
@@ -232,36 +236,36 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 
 	while (true)
 	{		
-		if (generation == 0)
+		if (m_generation == 0)
 		{
-			if (!(r = evolver.check(populations)))
+			if (!(r = evolver.check(m_populations)))
 				return "Error in population evolver check: " + r.getErrorString();
 		}
 
-		if (!(r = evolver.createNewPopulations(generation, populations, popSizes)))
+		if (!(r = evolver.createNewPopulations(m_generation, m_populations, popSizes)))
 			return "Error creating new population: " + r.getErrorString();
 
 		if (!(r = popSizeCheck()))
 			return r;
 
-		generation++; // At this point we can call it the new generation
+		m_generation++; // At this point we can call it the new generation
 
 		if (!(r = beforeFitnessCalculatedCallback()))
 			return r;
 
-		if (!(r = fitnessCalc.calculatePopulationFitness(populations)))
+		if (!(r = fitnessCalc.calculatePopulationFitness(m_populations)))
 			return "Error calculating fitness: " + r.getErrorString();
 
 		if (!(r = fitnessCalculatedCallback()))
 			return r;
 
-		if (generation == 1) // Have already increased it, this is the first time
+		if (m_generation == 1) // Have already increased it, this is the first time
 		{
-			if (!(r = migrationStrategy.check(populations)))
+			if (!(r = migrationStrategy.check(m_populations)))
 				return "Error in migration strategy check: " + r.getErrorString();
 		}
 		
-		if (!(r = migrationStrategy.migrate(generation, populations)))
+		if (!(r = migrationStrategy.migrate(m_generation, m_populations)))
 			return "Error in migration step: " + r.getErrorString();
 
 		if (!(r = popSizeCheck())) // Perhaps the amount changed, check it again!
@@ -269,13 +273,13 @@ bool_t EvolutionaryAlgorithm::run(IndividualCreation &gfc,
 
 		bool shouldStop = false;
 
-		if (!(r = stopCriterion.analyze(evolver, generation, shouldStop)))
+		if (!(r = stopCriterion.analyze(evolver, m_generation, shouldStop)))
 			return "Error in termination check: " + r.getErrorString();
 		if (shouldStop)
 			break;
 	}
 
-	if (!(r = onAlgorithmDone(generation, evolver.getBestIndividuals())))
+	if (!(r = onAlgorithmDone(m_generation, evolver.getBestIndividuals())))
 		return "Error inspecting best individuals upon algorithm end: " + r.getErrorString();
 
 	return true;
--- src/evolutionaryalgorithm.h
+++ src/evolutionaryalgorithm.h
@@ -32,12 +32,20 @@ public:
 			   const std::vector<size_t> &popSizes,
 			   const std::vector<size_t> &minPopulationSizes = std::vector<size_t>(),
 			   const std::vector<size_t> &maxPopulationSizes = std::vector<size_t>());
+
+	size_t getNumberOfGenerations() const { return m_generation; }
+	const std::shared_ptr<Population> &getPopulation() const { return m_population; }
+	const std::vector<std::shared_ptr<Population>> &getPopulations() const { return m_populations; }
 protected:
 	virtual errut::bool_t onBeforeFitnessCalculation(size_t generation, const std::shared_ptr<Population> &population) { return true; }
 	virtual errut::bool_t onFitnessCalculated(size_t generation, const std::shared_ptr<Population> &population) { return true; }
 	virtual errut::bool_t onBeforeFitnessCalculation(size_t generation, const std::vector<std::shared_ptr<Population>> &populations) { return true; }
 	virtual errut::bool_t onFitnessCalculated(size_t generation, const std::vector<std::shared_ptr<Population>> &populations) { return true; }
 	virtual errut::bool_t onAlgorithmDone(size_t generation, const std::vector<std::shared_ptr<Individual>> &bestIndividuals) { return true; }
+private:
+	std::vector<std::shared_ptr<Population>> m_populations;
+	std::shared_ptr<Population> m_population;
+	size_t m_generation;
 };
 
 }
