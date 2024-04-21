16852c16c53034faf8ac5d08b7ba67f5514c4541
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-04-21 09:25:38 +0200

    Clearing the current best set
    So that a new ND set can be restored without merging with this
    best set again

--- src/ndsortedpopulation.h
+++ src/ndsortedpopulation.h
@@ -16,6 +16,8 @@ public:
 					   bool bestOnlyDuplicateRemoval = true);
 	~NDSortedPopulation();
 
+	void clearBest() { m_best.resize(0); }
+
 	errut::bool_t check(const Population &population) override;
 	errut::bool_t processPopulation(const std::shared_ptr<Population> &population, size_t targetPopulationSize) override;
 	const std::vector<std::shared_ptr<Individual>> &getBestIndividuals() const override { return m_best; }
