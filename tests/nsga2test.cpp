63b8e8ffc1fe57f7be7d4d07514fcd3cffc7950b
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2024-08-06 17:11:50 +0200

    Some cleanup after using the functions from testfunctions.h

--- tests/nsga2test.cpp
+++ tests/nsga2test.cpp
@@ -20,34 +20,53 @@ using namespace eatk;
 using namespace std;
 using namespace errut;
 
-class BaseCalculation : public GenomeFitnessCalculation
+class ProblemBase : public GenomeFitnessCalculation
 {
 public:
-	BaseCalculation(size_t N, size_t M, size_t popSize, size_t numGen) : m_N(N), m_M(M), m_popSize(popSize), m_numGen(numGen) { }
-	size_t getDimension() const { return m_N; }
-	size_t getObjectives() const { return m_M; }
+	virtual size_t getDimension() const = 0;
+	virtual size_t getObjectives() const = 0;
+	virtual size_t getNumberOfGenerations() const = 0;
+	virtual size_t getPopulationSize() const = 0;
+	virtual void getBounds(size_t dim, double &xMin, double &xMax) = 0;
+	virtual std::vector<double> getVariableVectorFromGenome(const Genome &genome0) = 0;
+};
+
+template <class T>
+class BaseCalculationTemplate : public ProblemBase
+{
+public:
+	template <typename ... Args>
+	BaseCalculationTemplate(size_t popSize, size_t numGen,
+							Args&& ... args)
+		: m_popSize(popSize), m_numGen(numGen), m_f(std::forward<Args>(args)...) { }
+
+	size_t getDimension() const override { return m_f.getDimensions(); }
+	size_t getObjectives() const override { return m_f.getNumObjectives(); }
 
 	size_t getNumberOfGenerations() const { return m_numGen; }
 	size_t getPopulationSize() const { return m_popSize; }
-
-	void getBounds(size_t dim, double &xMin, double &xMax)
+	
+	void getBounds(size_t dim, double &xMin, double &xMax) override
 	{
-		if (dim >= m_N)
+		if (dim >= m_f.getDimensions())
 			throw runtime_error("Invalid dimension");
-		xMin = numeric_limits<double>::quiet_NaN();
-		xMax = numeric_limits<double>::quiet_NaN();
-		bounds(dim, xMin, xMax);
+		
+		auto [lower,upper] = m_f.getInitialParameterRange();
+		xMin = lower.at(dim);
+		xMax = upper.at(dim);
+
 		if (isnan(xMin) || isnan(xMax))
 			throw runtime_error("Bounds not filled in");
 	}
 
-	std::vector<double> getVariableVectorFromGenome(const Genome &genome0)
+	std::vector<double> getVariableVectorFromGenome(const Genome &genome0) override
 	{
 		assert(dynamic_cast<const DoubleVectorGenome *>(&genome0));
 		const DoubleVectorGenome &genome = static_cast<const DoubleVectorGenome &>(genome0);
 
-		if (genome.getValues().size() != m_N)
-			throw runtime_error("Expecting genome of size " + to_string(m_N) + " but got " + to_string(genome.getValues().size()));
+		auto N = m_f.getDimensions();
+		if (genome.getValues().size() != N)
+			throw runtime_error("Expecting genome of size " + to_string(N) + " but got " + to_string(genome.getValues().size()));
 
 		return genome.getValues();
 	}
@@ -76,106 +95,84 @@ public:
 
 		if (penalty == 0)
 		{
-			auto fv = calculate(v);
+			auto fv = m_f.calculate(v);
 			fitness.getValues() = fv;
 		}
 		else
 		{
-			vector<double> fv(m_M, penalty);
+			vector<double> fv(m_f.getNumObjectives(), penalty);
 			fitness.getValues() = fv;
 		}
 
-		if (fitness.getValues().size() != m_M)
-			throw runtime_error("Expecting " + to_string(m_M) + " objective values but got " + to_string(fitness.getValues().size()));
+		auto M = m_f.getNumObjectives();
+		if (fitness.getValues().size() != M)
+			throw runtime_error("Expecting " + to_string(M) + " objective values but got " + to_string(fitness.getValues().size()));
 
 		return true;
 	}
-
-protected:
-	virtual vector<double> calculate(const vector<double> &x) = 0;
-	virtual void bounds(size_t dim, double &xMin, double &xMax) = 0;
 private:
-	size_t m_N, m_M, m_popSize, m_numGen;
-};
-
-template <class T>
-class BaseCalculationTemplate : public BaseCalculation
-{
-public:
-	template <typename ... Args>
-	BaseCalculationTemplate(size_t dim, size_t numObj, size_t popSize, size_t numGen,
-							Args&& ... args)
-		: BaseCalculation(dim, numObj, popSize, numGen), m_f(std::forward<Args>(args)...) { }
-	
-	vector<double> calculate(const vector<double> &x) override { return m_f.calculate(x); }
-	void bounds(size_t dim, double &xMin, double &xMax) override
-	{
-		auto [lower,upper] = m_f.getInitialParameterRange();
-		xMin = lower.at(dim);
-		xMax = upper.at(dim);
-	}
-private:
+	size_t m_popSize, m_numGen;
 	T m_f;
 };
 
 class Rosenbrock : public BaseCalculationTemplate<eatk::testfunctions::Rosenbrock>
 {
 public:
-	Rosenbrock() : BaseCalculationTemplate(2, 1, 64, 2000, pair(-2.048, 2.048)) { }
+	Rosenbrock() : BaseCalculationTemplate(64, 2000, pair(-2.048, 2.048)) { }
 };
 
 class Griewank : public BaseCalculationTemplate<eatk::testfunctions::Griewank>
 {
 public:
-	Griewank() : BaseCalculationTemplate(10, 1, 64, 2000, 10, pair(-400.0, 400.0)) { }
+	Griewank() : BaseCalculationTemplate(64, 2000, 10, pair(-400.0, 400.0)) { }
 };
 
 class SCH : public BaseCalculationTemplate<eatk::testfunctions::Schaffer>
 {
 public:
-	SCH() : BaseCalculationTemplate(1, 2, 64, 2000, pair(-1000.0, 1000.0)) { }
+	SCH() : BaseCalculationTemplate(64, 2000, pair(-1000.0, 1000.0)) { }
 };
 
 class FON : public BaseCalculationTemplate<eatk::testfunctions::FonsecaFleming>
 {
 public:
-	FON() : BaseCalculationTemplate(3, 2, 64, 2000, pair(-4.0, 4.0)) { }
+	FON() : BaseCalculationTemplate(64, 2000, pair(-4.0, 4.0)) { }
 };
 
 class POL : public BaseCalculationTemplate<eatk::testfunctions::Poloni>
 {
 public:
-	POL() : BaseCalculationTemplate(2, 2, 64, 2000, pair(-M_PI, M_PI)) { }
+	POL() : BaseCalculationTemplate(64, 2000, pair(-M_PI, M_PI)) { }
 };
 
 class KUR : public BaseCalculationTemplate<eatk::testfunctions::Kursawe>
 {
 public:
-	KUR() : BaseCalculationTemplate(3, 2, 64, 2000, pair(-5.0, 5.0)) { }
+	KUR() : BaseCalculationTemplate(64, 2000, pair(-5.0, 5.0)) { }
 };
 
 class ZDT1 : public BaseCalculationTemplate<eatk::testfunctions::ZitzlerDebThiele1>
 {
 public:
-	ZDT1() : BaseCalculationTemplate(30, 2, 128, 2000, pair(0.0, 1.0)) { }
+	ZDT1() : BaseCalculationTemplate(128, 2000, pair(0.0, 1.0)) { }
 };
 
 class ZDT2 : public BaseCalculationTemplate<eatk::testfunctions::ZitzlerDebThiele2>
 {
 public:
-	ZDT2() : BaseCalculationTemplate(30, 2, 256, 2000, pair(0.0, 1.0)) { }
+	ZDT2() : BaseCalculationTemplate(256, 2000, pair(0.0, 1.0)) { }
 };
 
 class ZDT3 : public BaseCalculationTemplate<eatk::testfunctions::ZitzlerDebThiele3>
 {
 public:
-	ZDT3() : BaseCalculationTemplate(30, 2, 128, 2000, pair(0.0, 1.0)) { }
+	ZDT3() : BaseCalculationTemplate(128, 2000, pair(0.0, 1.0)) { }
 };
 
 class ZDT4 : public BaseCalculationTemplate<eatk::testfunctions::ZitzlerDebThiele4>
 {
 public:
-	ZDT4() : BaseCalculationTemplate(10, 2, 128, 2000, 
+	ZDT4() : BaseCalculationTemplate(128, 2000, 
 		vector{0.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0,-5.0 },
 		vector{1.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 }) { }
 };
@@ -183,7 +180,7 @@ public:
 class ZDT6 : public BaseCalculationTemplate<eatk::testfunctions::ZitzlerDebThiele6>
 {
 public:
-	ZDT6() : BaseCalculationTemplate(10, 2, 128, 2000, pair(0.0, 1.0)) { }
+	ZDT6() : BaseCalculationTemplate(128, 2000, pair(0.0, 1.0)) { }
 };
 
 // TODO: copied this for now, put in common file
@@ -225,7 +222,7 @@ private:
 class IndCreation : public IndividualCreation
 {
 public:
-	IndCreation(const std::shared_ptr<BaseCalculation> &calc, const shared_ptr<RandomNumberGenerator> &rng)
+	IndCreation(const std::shared_ptr<ProblemBase> &calc, const shared_ptr<RandomNumberGenerator> &rng)
 		: m_problem(calc), m_rng(rng)
 	{
 		m_N = calc->getDimension();
@@ -252,11 +249,11 @@ public:
 	}
 
 	size_t m_N, m_M;
-	shared_ptr<BaseCalculation> m_problem;
+	shared_ptr<ProblemBase> m_problem;
 	shared_ptr<RandomNumberGenerator> m_rng;
 };
 
-std::string getIndividualString(const shared_ptr<Individual> &ind, const shared_ptr<BaseCalculation> &problem)
+std::string getIndividualString(const shared_ptr<Individual> &ind, const shared_ptr<ProblemBase> &problem)
 {
 	string s = "genome: ";
 
@@ -269,7 +266,7 @@ std::string getIndividualString(const shared_ptr<Individual> &ind, const shared_
 class Stop : public FixedGenerationsStopCriterion
 {
 public:
-	Stop(size_t numGen, const shared_ptr<BaseCalculation> &problem)
+	Stop(size_t numGen, const shared_ptr<ProblemBase> &problem)
 		: FixedGenerationsStopCriterion(numGen), m_problem(problem) { }
 	
 	errut::bool_t analyze(const PopulationEvolver &evolver, size_t generationNumber, bool &shouldStop) override
@@ -281,7 +278,7 @@ public:
 		return FixedGenerationsStopCriterion::analyze(evolver, generationNumber, shouldStop);
 	}
 
-	shared_ptr<BaseCalculation> m_problem;
+	shared_ptr<ProblemBase> m_problem;
 };
 
 int mainCxx(const vector<string> &args)
@@ -292,7 +289,7 @@ int mainCxx(const vector<string> &args)
 		seed = (unsigned int)stoul(getenv("SEED"));
 	shared_ptr<RandomNumberGenerator> rng = make_shared<MersenneRandomNumberGenerator>(seed);
 
-	map<string, shared_ptr<BaseCalculation>> problems = {
+	map<string, shared_ptr<ProblemBase>> problems = {
 		{ "Rosenbrock", make_shared<Rosenbrock>() },
 		{ "Griewank", make_shared<Griewank>() },
 		{ "SCH", make_shared<SCH>() },
